6466eaa7eab22ce0530ed141b21688af6a5abe0b
paohui0134 <564954180@qq.com>
2016-07-04 11:45:42 +0800

    简单的搭建界面

--- PortMapping/MainDlg.cpp
+++ PortMapping/MainDlg.cpp
@@ -1,8 +1,10 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "MainDlg.h"
 #include "resource.h"
 
-CMainDlg::CMainDlg()
+CMainDlg::CMainDlg() :
+	m_pLeft_hide(nullptr), m_pBottom_hide(nullptr), m_pLeft_layout(nullptr),
+	m_pMapping_List(nullptr), m_pConnect_List(nullptr), m_pMenu_hide(nullptr)
 {
 }
 
@@ -15,7 +17,7 @@ CDuiString CMainDlg::GetSkinFolder()
 {
 	return L"";
 }
-//����xml�ļ���Դ����ԴID
+//返回xml文件资源的资源ID
 CDuiString CMainDlg::GetSkinFile()
 {
 	return L"skin.xml";
@@ -26,3 +28,70 @@ LPCTSTR CMainDlg::GetWindowClassName(void) const
 	return L"CMainDlg_Duilib";
 }
 
+void CMainDlg::InitWindow()
+{
+	//获取控件指针
+	CControlUI* pCur = nullptr;
+	pCur = m_PaintManager.FindSubControlByName(nullptr, L"btn_left");
+	if (pCur->GetInterface(DUI_CTR_BUTTON))
+	{
+		m_pLeft_hide = static_cast<CButtonUI*>(pCur);
+		m_pLeft_hide->OnNotify += MakeDelegate(this, &CMainDlg::ButtonNotify);
+	}
+		
+
+	pCur = m_PaintManager.FindSubControlByName(nullptr, L"btn_bottom");
+	if (pCur->GetInterface(DUI_CTR_BUTTON))
+	{
+		m_pBottom_hide = static_cast<CButtonUI*>(pCur);
+		m_pBottom_hide->OnNotify += MakeDelegate(this, &CMainDlg::ButtonNotify);
+	}
+
+	pCur = m_PaintManager.FindSubControlByName(nullptr, L"menubtn");
+	if (pCur->GetInterface(DUI_CTR_BUTTON))
+	{
+		m_pMenu_hide = static_cast<CButtonUI*>(pCur);
+		m_pMenu_hide->OnNotify += MakeDelegate(this, &CMainDlg::ButtonNotify);
+	}
+		
+	pCur = m_PaintManager.FindSubControlByName(nullptr, L"mapping_list");
+	if (pCur->GetInterface(DUI_CTR_LIST))
+	{
+		m_pMapping_List = static_cast<CListUI*>(pCur);
+	}
+
+	pCur = m_PaintManager.FindSubControlByName(nullptr, L"connect_list");
+	if (pCur->GetInterface(DUI_CTR_LIST))
+	{
+		m_pConnect_List = static_cast<CListUI*>(pCur);
+	}
+	m_pLeft_layout = m_PaintManager.FindSubControlByName(nullptr, L"left_layout");
+}
+
+bool CMainDlg::ButtonNotify(void* pNotify)
+{
+	TNotifyUI* pNotifyUI = (TNotifyUI*)pNotify;
+	if (!pNotifyUI)
+		return true;
+	if (pNotifyUI->sType == DUI_MSGTYPE_CLICK)//按钮单击
+	{
+		bool bvisible;
+		if (pNotifyUI->pSender == m_pLeft_hide)
+		{
+			bvisible = m_pLeft_layout->IsVisible();
+			m_pLeft_layout->SetVisible(!bvisible);
+			m_pLeft_hide->SetText(!bvisible ? L"◀" : L"▶");
+		}
+		else if (pNotifyUI->pSender == m_pBottom_hide)
+		{
+			bvisible = m_pConnect_List->IsVisible();
+			m_pConnect_List->SetVisible(!bvisible);
+			m_pBottom_hide->SetText(!bvisible ? L"▼" : L"▲");
+		}
+		else if (pNotifyUI->pSender == m_pMenu_hide)
+		{
+			
+		}
+	}
+	return true;
+}
--- PortMapping/MainDlg.h
+++ PortMapping/MainDlg.h
@@ -12,5 +12,18 @@ protected:
 	virtual LPCTSTR GetWindowClassName(void) const;//注册用的窗口类名
 public:
 	//重写基类虚函数
+	virtual void InitWindow() override;//在OnCreate最后调用
+
+
+private:
+	CButtonUI*	m_pLeft_hide;		//左边隐藏 显示按钮
+	CButtonUI*  m_pBottom_hide;		//下列表隐藏  显示按钮
+	CButtonUI*  m_pMenu_hide;		//设置按钮
+	CControlUI* m_pLeft_layout;		//左边添加信息的区域
+
+	CListUI* m_pMapping_List;		//映射关系列表
+	CListUI* m_pConnect_List;		//链接信息列表
+private:
+	bool ButtonNotify(void* pNotify);//按钮相应消息
 };
 
